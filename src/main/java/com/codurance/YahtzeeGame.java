c136f9f848f3ece65d3f807425c60a43d44a9de3
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-08-19 11:56:00 +0100

    Passing test for pairs category

--- src/main/java/com/codurance/YahtzeeGame.java
+++ src/main/java/com/codurance/YahtzeeGame.java
@@ -1,6 +1,7 @@
 package com.codurance;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class YahtzeeGame {
 
@@ -18,9 +19,36 @@ public class YahtzeeGame {
                 return multiplesOfN(rolls, 5);
             case "sixes":
                 return multiplesOfN(rolls, 6);
+            case "pairs":
+                return pairs(rolls);
             default:
                 return chance(rolls);
         }
+    }
+
+    private int pairs(int[] rolls) {
+        Map<Integer, Integer> rollCount = new HashMap<Integer, Integer>();
+        for (int roll : rolls) {
+            if(rollCount.containsKey(roll))
+                rollCount.replace(roll, rollCount.get(roll) + 1);
+            rollCount.put(roll, 1);
+        }
+
+        List<Integer> pairs = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> roll : rollCount.entrySet()) {
+            if(roll.getValue() == 1)
+                pairs.add(roll.getKey());
+        }
+
+
+        Integer largest = pairs.get(0);
+        for (Integer roll :
+                pairs) {
+            if(roll > largest)
+                largest = roll;
+        }
+
+        return largest * 2;
     }
 
     public int chance(int[] rolls) {
--- src/test/java/com/codurance/YahtzeeGameShould.java
+++ src/test/java/com/codurance/YahtzeeGameShould.java
@@ -31,7 +31,7 @@ public class YahtzeeGameShould {
                 Arguments.of(new int[]{6,3,3,4,4}, "fours", 8),
                 Arguments.of(new int[]{6,3,3,5,5}, "fives", 10),
                 Arguments.of(new int[]{6,3,3,5,5}, "sixes", 6),
-                Arguments.of(new int[]{3,3,3,4,4}, "pair", 8)
+                Arguments.of(new int[]{3,3,3,4,4}, "pairs", 8)
         );
     }
 
