627fd52a7e6139889c6a08004bdea1387baa59b9
AurelijaZuba <auryte00@hotmail.com>
2019-08-19 11:03:11 +0100

    refactoring - extract ones method

--- src/main/java/com/codurance/YahtzeeGame.java
+++ src/main/java/com/codurance/YahtzeeGame.java
@@ -3,15 +3,20 @@ package com.codurance;
 import java.util.Arrays;
 
 public class YahtzeeGame {
+
+    public int score(int[] rolls, String category) {
+        if(category.equals("ones")){
+            return ones(rolls);
+        }
+        return chance(rolls);
+    }
+
     public int chance(int[] rolls) {
         return Arrays.stream(rolls).reduce(0, (a, b) -> a + b);
     }
 
-    public int score(int[] rolls, String category) {
-        if(category.equals("ones")){
-           return Arrays.stream(rolls).filter(roll -> roll == 1)
-                   .reduce(0, (a,b) -> a + b);
-        }
-        return chance(rolls);
+    private int ones(int[] rolls) {
+        return Arrays.stream(rolls).filter(roll -> roll == 1)
+                .reduce(0, (a,b) -> a + b);
     }
 }
