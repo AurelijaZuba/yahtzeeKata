357c1d853bcf41f66d3c66bbabdb530f46a4fd2f
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-08-19 14:50:12 +0100

    Refactor - inline instatiation of Rule objects

--- src/main/java/com/codurance/YahtzeeGame.java
+++ src/main/java/com/codurance/YahtzeeGame.java
@@ -1,40 +1,29 @@
 package com.codurance;
 
-import java.util.Arrays;
-
 public class YahtzeeGame {
 
     private final Chance chance = new Chance();
-    private final ThreeOfAKind threeOfAKind = new ThreeOfAKind();
-    private final TwoPairs twoPairs = new TwoPairs();
-    private final Pairs pairs = new Pairs();
-    private final MultiplesOfOne multiplesOfOne = new MultiplesOfOne();
-    private final MultiplesOfTwo multiplesOfTwo = new MultiplesOfTwo();
-    private final MultipleOfThrees multipleOfThrees = new MultipleOfThrees();
-    private final MultipleOfFours multipleOfFours = new MultipleOfFours();
-    private final MultipleOfFives multipleOfFives = new MultipleOfFives();
-    private final MultipleOfSixes multipleOfSixes = new MultipleOfSixes();
 
     public int score(int[] rolls, String category) {
         switch (category) {
             case "ones":
-                return multiplesOfOne.score(rolls);
+                return new MultiplesOfOne().score(rolls);
             case "twos":
-                return multiplesOfTwo.score(rolls);
+                return new MultiplesOfTwo().score(rolls);
             case "threes":
-                return multipleOfThrees.score(rolls);
+                return new MultipleOfThrees().score(rolls);
             case "fours":
-                return multipleOfFours.score(rolls);
+                return new MultipleOfFours().score(rolls);
             case "fives":
-                return multipleOfFives.score(rolls);
+                return new MultipleOfFives().score(rolls);
             case "sixes":
-                return multipleOfSixes.score(rolls);
+                return new MultipleOfSixes().score(rolls);
             case "pairs":
-                return pairs.score(rolls);
+                return new Pairs().score(rolls);
             case "two pairs":
-                return twoPairs.score(rolls);
+                return new TwoPairs().score(rolls);
             case "three of a kind":
-                return threeOfAKind.score(rolls);
+                return new ThreeOfAKind().score(rolls);
             case "four of a kind":
                 return new FourOfAKind().score(rolls);
             default:
